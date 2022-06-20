ff35c2ec6c5c0dbb5d95045daf7df3814fbb4862
emonShimoul <emonshimoul@gmail.com>
2022-06-20 10:44:41 +0600

    use mapbox direction

--- src/components/MapDirection.js
+++ src/components/MapDirection.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
+import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions'
+
+mapboxgl.accessToken = 'give here access token';
 
 const MapDirection = () => {
+    useEffect(()=>{
+        const map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [-79.4512, 43.6568],
+            zoom: 13
+            });
+             
+            map.addControl(
+            new MapboxDirections({
+            accessToken: mapboxgl.accessToken
+            }),
+            'top-left'
+            );
+    },[]);
     return (
         <div>
-            
+            <div id="map"></div>
         </div>
     );
 };
